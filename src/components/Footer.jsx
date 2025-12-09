4653d1ea7496e7138c8002d68af06788e34e0e74
Olakunle Samuel <olakunleolawale21@gmail.com>
2025-12-09 17:34:08 +0100

    second change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return <div className="attribution">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-    Coded by <a href="#">Olakunle Samuel</a>.
+    Coded by <a href="https://www.frontendmentor.io/profile/Ola-wale21">Olakunle Samuel</a>.
   </div>
 }
 export default Footer;
